f9fc0f682b67cb0a8604171d0d7e0071b1d1c88e
MuhammadMoiz200099 <muhammadmoiz0087@gmail.com>
2024-05-31 23:35:46 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Navigation from "./navigation/navigation";
 import Loader from './components/loader/loader';
 
 function App() {
+  const [openView, setOpenView] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setOpenView(true);
+    }, (300));
+  }, [])
   return (
     <>
       <Loader />
-      <Navigation />
+      {openView && (
+        <Navigation />
+      )}
     </>
   );
 }
